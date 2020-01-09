1908bf540a8dbb8322fb01261fba7f19d38e84ff
Dowon Lee <edowon0623@gmail.com>
2020-01-09 13:35:18 +0900

    upload docker-compose file for mongodb replication setting

---
+++ 04.Docker/day03/mongo/replicaSet.js
@@ -0,0 +1,11 @@
+config = {
+    _id: "myapp",
+    members: [
+        {_id:0, host: "mongo1:27017"},
+        {_id:1, host: "mongo2:27017"},
+        {_id:2, host: "mongo3:27017"},
+    ]
+}
+
+rs.initiate(config);
+rs.conf();
